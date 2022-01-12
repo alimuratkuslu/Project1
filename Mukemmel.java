832d9d2c8225bb59921a427bc783d89eec994b01
alimuratkuslu <alimuratkuslu@gmail.com>
2022-01-12 23:20:04 +0300

    commit 26

---
+++ Mukemmel.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Mukemmel {
+    public static void main(String[]args){
+
+        Scanner inp = new Scanner(System.in);
+
+        int sayi;
+
+        System.out.println("Bir sayı giriniz: ");
+        sayi = inp.nextInt();
+
+        int sum = 0;
+
+        for(int i = 1; i < sayi; i++){
+            if(sayi % i == 0){
+                sum += i;
+            }
+        }
+
+        if(sum == sayi){
+            System.out.println(sayi + " bir mükemmel sayıdır");
+        }
+        else{
+            System.out.println(sayi + " bir mükemmel sayı değildir");
+        }
+    }
+}
